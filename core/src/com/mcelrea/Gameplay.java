d3ce7b2e9f8767d4062f7d33271ef6dc8508d0d3
mcelrea <mcelrea@damien-hs.edu>
2015-04-15 12:39:53 -0700

    Added stuff to gameplay

--- core/src/com/mcelrea/Gameplay.java
+++ core/src/com/mcelrea/Gameplay.java
@@ -3,26 +3,47 @@ package com.mcelrea;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 
 /**
  * Created by Tech on 2/19/2015.
  */
 public class Gameplay implements Screen {
 
+    World world;
+    Box2DDebugRenderer debugRenderer;
+    SpriteBatch batch;
+    OrthographicCamera camera;
+
     @Override
     public void show() {
-
+        world = new World(new Vector2(0,-9.81f), true);
+        debugRenderer = new Box2DDebugRenderer();
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+
+        batch.begin();
+        batch.end();
+
+        debugRenderer.render(world, camera.combined);
     }
 
     @Override
     public void resize(int width, int height) {
-
+        camera.viewportWidth = width/25f;
+        camera.viewportHeight = height/25f;
     }
 
     @Override
